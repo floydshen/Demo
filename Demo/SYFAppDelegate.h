7fbc845a8430ef2055797092ae7501c84b1a2830
Floyd Shen <floyd.shen@yahoo.com>
2013-05-15 22:03:12 +0800

    Initial Commit

---
+++ Demo/SYFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SYFAppDelegate.h
+//  Demo
+//
+//  Created by floyd on 13-5-15.
+//  Copyright (c) 2013年 floyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by floyd on 13-5-15.
+//  Copyright (c) 2013年 floyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SYFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SYFAppDelegate class]));
+    }
+}
