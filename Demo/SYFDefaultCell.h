2974b18d34f3b224274480034a7381a43b83a6b1
floydshen <floyd.shen@gmail.com>
2013-05-18 22:55:50 +0800

    add default cell

---
+++ Demo/SYFDefaultCell.h
@@ -0,0 +1,13 @@
+//
+//  SYFDefaultCell.h
+//  Demo
+//
+//  Created by floyd on 13-5-18.
+//  Copyright (c) 2013年 floydshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYFDefaultCell : UITableViewCell
+
+@end
